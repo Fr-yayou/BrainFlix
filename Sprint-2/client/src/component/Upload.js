f917c13efdaaf90b95b20fe626839173da2adedd
Marianna Cherniuk <Marianna@Naomis-MacBook.localdomain>
2019-08-12 14:01:08 -0700

    FINAL

--- Sprint-2/client/src/component/Upload.js
+++ Sprint-2/client/src/component/Upload.js
@@ -1,10 +1,42 @@
 import React from "react";
 import thumbnail from "../assets/image/Upload-video-preview.jpg";
+import axios from "axios"
+
+const newApi = `http://localhost:8080/videos`;
 
 
-function Upload () {
-    return (
-        <div className="container-upload">
+
+class Upload extends React.Component{
+
+    state = {
+            title: '',
+            description: ''
+        };
+
+	postNew = () => {
+		axios
+			.post(newApi, {
+                title: this.state.title,
+				description: this.state.description
+			})
+			.then(response => {
+                console.log(response);
+                console.log(this.state.title)
+                console.log(this.state.description)
+			});
+    };
+    onSubmit = event => {
+        event.preventDefault()
+    }
+
+	onChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+    render() {
+        return (
+            <div className="container-upload">
             <h5 className="container-upload__title">Upload Video</h5>
             <div id="line-two"></div>
             <div className="container-thumbnail">
@@ -12,20 +44,20 @@ function Upload () {
                 <img className="container-thumbnail__title__video" src={thumbnail} alt="video"/>
             </div>
             <div>
-                <form>
+                <form onSubmit={this.onSubmit} >
                     <div className="container-formTwo"> 
                         <label className="container-formTwo__title">TITLE YOUR VIDEO</label>
-                        <input id="color" className="container-formTwo__title__com" type="text" placeholder="Add a title to your video"/>
+                        <input id="color" className="container-formTwo__title__com" type="text" placeholder="Add a title to your video" name="title" onChange={this.onChange}/>
                     </div>
                     <div>
                         <label className="container-formTwo__title__com__titleTwo">ADD A VIDEO DESCRIPTION</label>
-                        <input id="color" className="container-formTwo__title__com__titleTwo__description" type="text" placeholder="Add a description of your video"/>
+                        <input id="color" className="container-formTwo__title__com__titleTwo__description" type="text" placeholder="Add a description of your video" name="description" onChange={this.onChange}/>
                     </div>
 
                     <div className="container-button">
                         <div id="line-two"></div>
                         <div id="h">
-                        <button id="btn" className="container-button__publish">PUBLISH</button>
+                        <button id="btn" className="container-button__publish" onClick={this.postNew}>PUBLISH</button>
                         <div id="center">
                         <h5 className="container-button__publish__cancel">Cancel</h5>
                         </div>
@@ -35,8 +67,9 @@ function Upload () {
             </div>
 
         </div>
-        
-    )
+        )
+    }
 }
+
 
 export default Upload;
--- Sprint-2/server/routes/routes.js
+++ Sprint-2/server/routes/routes.js
@@ -23,36 +23,38 @@ router.get('/:id', (req, res) => {
 	res.json(videoData);
 });
 
-// router.post('/', (req, res) => {
-//   console.log(req.body)
-// 	const singleVideo = {
-// 		id: uniqid(),
-// 		title: req.body.title,
-// 		channel: 'Tas Talks',
-// 		image: 'https://i.imgur.com/eknVBeg.jpg'
-// 	};
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const singleVideo = {
+        id: uniqid(),
+        title: req.body.title,
+        channel: 'Yanis',
+        image: 'https://www.tripsavvy.com/thmb/8740ZwofHCLGhbcC0N2K-R5bcdQ=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/mid-adult-man-skiing-down-mountain--golden--british-columbia--canada-480984215-5b50d2f2c9e77c005b1b87be.jpg'
+    };
 
-// 	const videoDetail = {
-// 		id: singleVideo.id,
-// 		title: req.body.title,
-// 		channel: 'Tas Talks',
-// 		image: 'https://i.imgur.com/eknVBeg.jpg',
-// 		description: req.body.description,
-// 		views: '5,001,023',
-// 		likes: '1,985',
-// 		duration: '00:10',
-// 		video: 'https://project-2-api.herokuapp.com/stream',
-//     timestamp: 1545162149000,
-//     comments:[]
-// 	};
+    const videoDetail = {
+        id: singleVideo.id,
+        title: req.body.title,
+        channel: 'Yanis',
+        image: 'https://www.tripsavvy.com/thmb/8740ZwofHCLGhbcC0N2K-R5bcdQ=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/mid-adult-man-skiing-down-mountain--golden--british-columbia--canada-480984215-5b50d2f2c9e77c005b1b87be.jpg',
+        description: req.body.description,
+        views: '6000',
+        likes: '30000',
+        duration: '5,95',
+        video: '',
+        timestamp:'',
+        comments: []
+    };
 
-// 	videos.push(singleVideo);
-// 	videoList.push(videoDetail);
+    videos.push(singleVideo);
+    videoList.push(videoDetail);
 
-// 	fs.writeFileSync('./routes/videos.json', JSON.stringify(videos));
-// 	fs.writeFileSync('./routes/videoList.json', JSON.stringify(videoList));
-// 	res.send(singleVideo);
-// 	res.send(videoDetail)
+    fs.writeFileSync('./routes/videos.json', JSON.stringify(videos));
+    fs.writeFileSync('./routes/videoList.json', JSON.stringify(videoList));
+    res.send(singleVideo);
+
+
+});
 
 
 module.exports = router;
